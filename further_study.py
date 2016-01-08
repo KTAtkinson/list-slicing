2b7abf366193806ffa6e6429c0032acb291a2498
Hackbright Student <info@hackbrightacademy.com>
2016-01-07 18:02:24 -0800

    More improvements to further_studdy

--- further_study.py
+++ further_study.py
@@ -107,8 +107,10 @@ def custom_remove(input_list, value):
     True
 
     """
-
-    for index in range(custom_len)
+    for index, item in enumerate(input_list):
+        if item == value:
+            del input_list[index]
+            break
 
 
 def custom_pop(input_list):
@@ -122,8 +124,9 @@ def custom_pop(input_list):
     'March'
 
     """
-
-    return None
+    popped_item = input_list[-1]
+    del input_list[-1]
+    return popped_item
 
 
 def custom_index(input_list, value):
@@ -138,7 +141,11 @@ def custom_index(input_list, value):
 
     """
 
-    return 0
+    for index, item in enumerate(input_list):
+        if item == value:
+            return index
+
+    print "You can't remove a value that's not there!"
 
 
 def custom_count(input_list, value):
@@ -152,8 +159,12 @@ def custom_count(input_list, value):
     2
 
     """
-
-    return 0
+    count = 0
+    for item in input_list:
+        if item == value:
+            count += 1
+    
+    return count
 
 
 def custom_reverse(input_list):
@@ -170,7 +181,7 @@ def custom_reverse(input_list):
 
     """
 
-    pass
+    list_copy = copy()
 
 
 def custom_contains(input_list, value):
